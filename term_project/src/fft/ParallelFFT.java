adf075d934e1a4565d425dab6d1d8707e9574a48
Trevor <trevor.anderson16@gmail.com>
2020-11-22 08:46:47 -0600

    low-memory version working

--- term_project/src/fft/ParallelFFT.java
+++ term_project/src/fft/ParallelFFT.java
@@ -13,6 +13,8 @@ public class ParallelFFT {
 
     // compute the FFT of x[], assuming its length n is a power of 2
     public static Complex[] fft(Complex[] x) {
+//    	System.out.println(set + " begin -------------------------");
+//    	indent += "\t";
         int n = x.length;
 
         // base case
@@ -40,11 +42,22 @@ public class ParallelFFT {
         // combine
         Complex[] y = new Complex[n];
         for (int k = 0; k < n/2; k++) {
+//        	System.out.println(indent + "k: " + k);
+//        	System.out.println(indent + "k + n/2: " + (k + n/2));
             double kth = -2 * k * Math.PI / n;
             Complex wk = new Complex(Math.cos(kth), Math.sin(kth));
+            System.out.println("-----------------------------------------------------");
+            System.out.println("k: " + k);
+            System.out.println("evenFFT[k]: " + evenFFT[k]);
+            System.out.println("oddFFT[k]: " + oddFFT[k]);
             y[k]       = evenFFT[k].plus (wk.times(oddFFT[k]));
+            System.out.println("y[k]       = evenFFT[k].plus (wk.times(oddFFT[k]));");
+            System.out.println("y[k]: " + y[k]);
             y[k + n/2] = evenFFT[k].minus(wk.times(oddFFT[k]));
+            System.out.println("y[k + n/2] = evenFFT[k].minus(wk.times(oddFFT[k]));");
+            System.out.println("y[k + n/2]: " + y[k + n/2]);
         }
+//        System.out.println(set + " end -------------------------");
         return y;
     }
 
@@ -58,11 +71,53 @@ public class ParallelFFT {
         System.out.println();
     }
     
+    public static void low_memory_fft(Complex[] x, int[] indices) {
+    	int n = indices.length;
+
+        // base case
+        if (n == 1) { return; };
+
+        // radix 2 Cooley-Tukey FFT
+        if (n % 2 != 0) {
+            throw new IllegalArgumentException("n is not a power of 2");
+        }
+
+        // compute FFT of even terms
+        int[] evenIndexes = new int[n/2];
+        for (int k = 0; k < n/2; k++) {
+        	evenIndexes[k] = indices[2*k];
+        }
+        
+        low_memory_fft(x, evenIndexes);
+
+        // compute FFT of odd terms
+        int[] oddIndexes = new int[n/2];;  // reuse the array (to avoid n log n space)
+        for (int k = 0; k < n/2; k++) {
+        	oddIndexes[k] = indices[2*k + 1];
+        }
+        low_memory_fft(x, oddIndexes);
+        
+        for (int k = 0; k < n/2; k++) {
+        	
+            double kth = -2 * k * Math.PI / n;
+            Complex wk = new Complex(Math.cos(kth), Math.sin(kth));
+            Complex temp1 = x[evenIndexes[k]];
+            Complex temp2 = x[oddIndexes[k]];
+            x[evenIndexes[k]]       = temp1.plus (wk.times(temp2));
+            x[oddIndexes[k]] = temp1.minus (wk.times(temp2));
+//            System.out.println("x[indices[k]]       = temp1.plus (wk.times(temp2));");
+//            System.out.println(x[indices[k]]);
+//            System.out.println("x[indices[k + n/2]] = temp1.minus (wk.times(temp2));");
+//            System.out.println(x[indices[k + n/2]]);
+        }
+    }
+    
     public static Complex[] generateSignal(int N)
 	{
 			Complex[] signal = new Complex[N];
 			for (int i = 0; i < N; i++)
 			{
+//				signal[i] = new Complex(i, 0);
 				signal[i] = new Complex(Math.cos((2 * i * 1 * Math.PI) / N) + 0.5
 						* Math.cos(((2 * i * 2 * Math.PI) / N) + Math.PI / 4), 0);
 			}
@@ -77,79 +132,92 @@ public class ParallelFFT {
     	for (int power = 2; power < 32; power++) {
     		long timeSum = 0;
     		int array_size = (int)Math.pow(2, power);
-    		for (int i = 0; i<100; i++) {
+    		int[] indexes = new int[array_size];
+    		for (int idx = 0; idx < array_size; idx++)
+    			indexes[idx] = idx;
+    		
+//    		for (int i = 0; i<100; i++) {
     		
 	    		Complex[] x = generateSignal(array_size);
 	
 	    		final long startTime = System.currentTimeMillis();
 	            // FFT of original data
-	            Complex[] y = fft(x);
+	            low_memory_fft(x, indexes);
 	            final long executionTime = System.currentTimeMillis() - startTime;
 	            timeSum += executionTime;
-    		}
-    		long timeAvg = timeSum / 100;
+//    		}
+    		long timeAvg = timeSum;// / 100;
             System.out.println(array_size + "," + timeAvg);
-            long total = rt.totalMemory();
-            long free = rt.freeMemory();
-            if (total != prevTotal || free != prevFree) {
-                long used = total - free;
-                long prevUsed = (prevTotal - prevFree);
-                System.out.println(
-                    "#" + power +
-                    ", Total: " + total +
-                    ", Used: " + used +
-                    ", ∆Used: " + (used - prevUsed) +
-                    ", Free: " + free +
-                    ", ∆Free: " + (free - prevFree));
-                prevTotal = total;
-                prevFree = free;
-            }
+//            long total = rt.totalMemory();
+//            long free = rt.freeMemory();
+//            if (total != prevTotal || free != prevFree) {
+//                long used = total - free;
+//                long prevUsed = (prevTotal - prevFree);
+//                System.out.println(
+//                    "#" + power +
+//                    ", Total: " + total +
+//                    ", Used: " + used +
+//                    ", ∆Used: " + (used - prevUsed) +
+//                    ", Free: " + free +
+//                    ", ∆Free: " + (free - prevFree));
+//                prevTotal = total;
+//                prevFree = free;
+//            }
             
     	}
     }
-    
-    public static void CooleyTukeyParallel() {
-    	Runtime rt = Runtime.getRuntime();
-        long prevTotal = 0;
-        long prevFree = rt.freeMemory();
-    	
-    	for (int power = 2; power < 32; power++) {
-    		long timeSum = 0;
-    		int array_size = (int)Math.pow(2, power);
-    		for (int i = 0; i<100; i++) {
-    		
-	    		Complex[] x = generateSignal(array_size);
-	
-	    		final long startTime = System.currentTimeMillis();
-	            // FFT of original data
-	            Complex[] y = fft(x);
-	            final long executionTime = System.currentTimeMillis() - startTime;
-	            timeSum += executionTime;
-    		}
-    		long timeAvg = timeSum / 100;
-            System.out.println(array_size + "," + timeAvg);
-            long total = rt.totalMemory();
-            long free = rt.freeMemory();
-            if (total != prevTotal || free != prevFree) {
-                long used = total - free;
-                long prevUsed = (prevTotal - prevFree);
-                System.out.println(
-                    "#" + power +
-                    ", Total: " + total +
-                    ", Used: " + used +
-                    ", ∆Used: " + (used - prevUsed) +
-                    ", Free: " + free +
-                    ", ∆Free: " + (free - prevFree));
-                prevTotal = total;
-                prevFree = free;
-            }
-            
-    	}
-    }
+//    
+//    public static void CooleyTukeyParallel() {
+//    	
+//    	for (int power = 2; power < 32; power++) {
+//    		long timeSum = 0;
+//    		int array_size = (int)Math.pow(2, power);
+//    		for (int i = 0; i<100; i++) {
+//    		
+//	    		Complex[] x = generateSignal(array_size);
+//	
+//	    		
+//	    		
+//	    		
+//	    		
+//	    		
+//	    		
+//	    		
+//	    		
+//	    		
+//	    		final long startTime = System.currentTimeMillis();
+//	            // FFT of original data
+//	            Complex[] y = fft(x);
+//	            final long executionTime = System.currentTimeMillis() - startTime;
+//	            timeSum += executionTime;
+//    		}
+//    		long timeAvg = timeSum / 100;
+//            System.out.println(array_size + "," + timeAvg);
+//            
+//    	}
+//    }
     
     public static void main(String[] args) { 
+
+//		int array_size = (int)Math.pow(2, 4);
+//		int[] indices = new int[array_size];
+//		Complex[] x = generateSignal(array_size);
+//		for (int i = 0; i<array_size;i++)
+//			indices[i] = i;
+//    	
+//    	Complex[] y = fft(x);
+//    	
+//    	System.out.println("======================================================================================"
+//    			+ "\n======================================================================================"
+//    			+ "\n======================================================================================"
+//    			+ "\n======================================================================================");
+//    	low_memory_fft(x, indices, "");
+//
+//    	show(y, "Original");
+//    	
+//    	show(x, "Modified");
     	CooleyTukeySeries();
-    	CooleyTukeyParallel();
+//    	CooleyTukeyParallel();
     }
 
 }
